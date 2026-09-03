697ee8a434254d3cf1599bc9da9cd0a60a7e69b2
dev-muneeb-26 <devmuneebshahzad@gmail.com>
2026-09-03 00:20:58 -0700

    Day-04 code material

--- Day-04/test.dart
+++ Day-04/test.dart
@@ -1,11 +1,27 @@
+double balance=5000; //global varibale
+
 void main(){
 
 String res=maxFinder(34,5);
 print(res);
 
 // mini bank
-// double balance=5000;
 
+greeet("Muneeb");
+
+int result=add(1,3);
+print(result);
+
+bool evn=isEven(10);
+print(evn);
+int sq=square(5);
+print(sq);
+int fin1=addition(1,2);
+int fin2=subtaction(1,2);
+int fin3=mul(1,2);
+double fin4=division(1,2);
+
+desposit(300);
 
 }
 
@@ -17,14 +33,45 @@ String maxFinder(int a,int b){
     }
 }
 
-// void desposit(double ammount){
-// balance+=ammount;
-// print("ammout added successfully");
-// }
+void desposit(double ammount){
+balance+=ammount;
+print("ammout added successfully");
+}
 
-// void withdraw(double ammount){
-//  balance-=ammount;
-//  print("ammount dected successfully");
-// } 
+void withdraw(double ammount){
+ balance-=ammount;
+ print("ammount dected successfully");
+} 
 
+ 
+void greeet(String name){
+    print("Hello ${name}");
+}
 
+int add(int a,int b){
+    return a+b;
+}
+
+int addition(int a,int b){
+    return a+b;
+}
+int subtaction(int a,int b){
+    return a-b;
+}
+
+int mul(int a,int b){
+    return a*b;
+}
+
+double division(double a,double b){
+    return a/b;
+}
+int square(int num)=> num*num;
+
+bool isEven(int num){
+    if(num%2==0){
+     return true;
+    }else {
+      return false;
+    }
+}
